05d9873648578b368231939222096a7bb7d80b30
datavers <73333701+datavers@users.noreply.github.com>
2022-09-29 14:25:32 +0200

    Update streamlit_app.py
    Added requests package

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,8 @@
 import streamlit
 import pandas as pd
+import requests as r
+
+fruitvice_response = r.get("https://fruityvice.com/api/fruit/watermelon")
 
 my_fruit_list = pd.read_csv('https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt')
 
@@ -22,3 +25,4 @@ my_fruit_list = my_fruit_list.set_index('Fruit')
 fruits_selected = streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index))
 fruits_to_show = my_fruit_list.loc[fruits_selected]
 streamlit.dataframe(fruits_to_show)
+streamlit.text(fruitvice_response)
